63c386df9b28493cb8b0aff638a0fb90b7c048ba
Karl Hallsby <karl@hallsby.com>
2021-07-28 00:44:43 -0500

    First implementation of adding @Article entry to file
    We start by getting the current buffer (the main bib file) and making
    an indirect buffer that is attached to the main buffer. From there,
    the main bib file is saved at the beginning of (save-current-buffer...
    The body of the form is evaluated, and the last thing the
    save-current-buffer form SHOULD do is switch us back to the main
    bibliography file.

--- lisp/transient-bib-entry.el
+++ lisp/transient-bib-entry.el
@@ -15,6 +15,29 @@
   (interactive)
   (transient-bib-UNIMPLEMENTED "transient-bib-entry-new-placeholder"))
 
+(defun transient-bib-entry-new-article ()
+  "Create a new article bibliography entry.
+
+Opens a new read/write capable buffer in `bibtex-mode' for entering the entry.
+Once done, the contents of the buffer are copied back to the main bibliography
+buffer and the file is automatically saved."
+  (interactive)
+  (let ((bib-file-buffer (current-buffer))
+        ;; (entry-buffer (get-buffer-create "transient-bib-entry"))
+        (entry-buffer (make-indirect-buffer (current-buffer) "transient-bib-entry" nil)))
+    (save-current-buffer
+      ;; TODO: Verification of parent buffer for indirect buffer?
+      (switch-to-buffer entry-buffer) ;; TODO: use pop-to-buffer-same-window instead?
+      (setq inhibit-read-only t)
+      (bibtex-mode)
+      (bibtex-Article)
+      ;; TODO: Wait until C-c C-c keybinding is pressed before finishing up here.
+      ;; NOTE: C-C C-c is bound to bibtex-clean-entry when in bibtex-mode
+      ;; (bibtex-clean-entry)
+      ;; (setq inhibit-read-only nil) ;; Make the entry buffer read-only again
+      ;; TODO: Ensure switching back to main bib file at end.
+      )))
+
 ;;;###autoload
 (defun transient-bib-entry-edit ()
   "Doing things wrong."
@@ -25,7 +48,7 @@
   "Create a new BibTeX/BibLaTeX entry."
   :info-manual "(transient-bib)New Entry"
   ["Entry Type"
-   ("a" "Article" bibtex-Article)
+   ("a" "Article" transient-bib-entry-new-article)
    ("p" "PLACEHOLDER" transient-bib-entry-new-placeholder)])
 
 (transient-define-prefix transient-bib-entry ()
