fd8a296fcc7928967cae05a814c0baefc4893b3a
Karl Hallsby <karl@hallsby.com>
2021-08-10 09:19:10 -0500

    Remove note that C-c C-c is bound already, because I now handle that

--- lisp/transient-bib-entry.el
+++ lisp/transient-bib-entry.el
@@ -58,8 +58,6 @@ buffer and the file is automatically saved."
       (bibtex-Article)
       ;; TODO: Need to verify that xref tag is unique among all entries in bibtex file
       ;; TODO: Wait until C-c C-c keybinding is pressed before finishing up here.
-      ;; NOTE: C-C C-c is bound to bibtex-clean-entry when in bibtex-mode
-      ;; (bibtex-clean-entry)
       ;; NOTE: If using with-temp-buffer, kill-buffer is called for me
       ;; (setq inhibit-read-only nil) ;; Make the entry buffer read-only again
       ;; TODO: Ensure switching back to main bib file at end.
